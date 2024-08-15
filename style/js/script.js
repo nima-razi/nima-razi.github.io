b2c38c757c1196610372d865fdb029df2b9a3328
Nima Razi <nima.razi@hotmail.com>
2024-08-15 12:56:16 +0300

    added icons for smaller screens and video background

--- style/js/script.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $("#about").load("about.html");
-    $("#experience").load("experience.html"); 
-    $("#education").load("education.html"); 
-    $("#projects").load("projects.html"); 
-    $("#misc").load("misc.html"); 
-});
